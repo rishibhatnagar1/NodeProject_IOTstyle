439f3f2ebb5b53725975eb0362c05d41cbccae24
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-02-20 18:07:19 +0530

    Working with python, tested

--- sendPhotostoServer/sendPic.py
+++ sendPhotostoServer/sendPic.py
@@ -1,10 +1,16 @@
 import requests
 import time
 
-url = 'http://httpbin.org/post'
-files ={'file':open('head.jpeg','rb')}
+url = 'http://localhost:3000/pagemulti'
 
-r = requests.post(url,files=files)
-print "posted"
+def fileName (cur_name,post_name):
+	files ={post_name:open(cur_name,'rb')}
+	r = requests.post(url,files=files)
+	if (r.status_code) ==200:
+		print "Posted"
+	else:
+		print r.status_code
 
-print r.text
+val = raw_input("Enter the current value of the file with the extension: ")
+val1 = raw_input ("Please enter the filename you want to keep after posting: ")
+fileName(val,val1)
