59d69b99a5ba365352ff294d7a53de8f4d803305
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-02-24 18:03:01 +0530

    ack functionalities now

--- sendPhotostoServer/server/picHolder.js
+++ sendPhotostoServer/server/picHolder.js
@@ -4,12 +4,19 @@ The code below is going to used to send files to a server and saving them there.
 var rest = require('exprestify') //Module required
 
 /*The following code defines where will the file be saved,how will it be named etc */
-var multiopt ={
-FilePath:"./images/", 		//File is going to be saved at a folder called 'images'
-PostType:"file", 		//Mulitpart post
-Rename: function(fieldname,filename){
-	return fieldname; //Field name is what you add to the file,you then control how the file will be saved in the folder
+var multiopt = {
+    FilePath: "./images/", //File is going to be saved at a folder called 'images'
+    PostType: "file", //Mulitpart post
+    Rename: function (fieldname, filename) {
+        return fieldname; //Field name is what you add to the file,you then control how the file will be saved in the folder
+    }
 }
+opt = {
+    extended: false
+}
+var options = {
+    contentType: "json",
+    config: opt
 }
 
 rest.multipost('/pagemulti', function (err, data) { //File will be posted at servername/pagemulti 
@@ -34,18 +41,56 @@ rest.getfile('/images', function (err, query) {
     }
 })
 /*The following will be used for telling the processor to do something it will have both post and get functionalities */
-rest.get('/command',function(err,data){
+var datafromApp = "none";
 
+rest.get('/command', function (err) {
+    if (!err) {
+
+        console.log("Ret: " + datafromApp);
+        return datafromApp;
+    } else {
+        console.log(err);
+        return err;
+    }
 });
 
+rest.post('/command', function (err, data) {
+    if (!err) {
+        datafromApp = data.value;
+        console.log("Ret: "+data.value + "success");
+        return datafromApp;
+    } else {
+        console.log(err);
+        return err;
+    }
+}, options);
+/*The following will be used to receive an acknowlegement from the server when an activity is over. This will just need a post and get functionality */
+var ackflag = "00";
+var ackstatus = "nostatus";
+rest.post('/ack', function (err, data) {
+    if (!err) {
+        ackstatus = data.value;
+        console.log("Ret: "+ackstatus);
+        return ackstatus;
+    } else {
+        return err;
+    }
+}, options);
 
-
-rest.post('/command',function(err,data){
-
-});
-/*The following will be used to receive an acknowlegement from the server when an activity is over. This will just need a get functionality */
-rest.get('/ack',function(err,data){
-
+rest.get('/ack', function (err) {
+    if (!err) {
+        if (ackstatus == "success") {
+            ackflag = "1"; //success
+        }
+        if (ackstatus == "failed") {
+            ackflag = "0"; //No success
+        }
+        return ackflag;
+        console.log("Ret: "+ackstatus);
+    } else {
+        console.log(err);
+        return err;
+    }
 });
 
 
